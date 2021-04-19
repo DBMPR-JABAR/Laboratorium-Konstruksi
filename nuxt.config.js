a2cc7cd56f132153c835f268ea6114b034955ab3
firmanmardiyanto <firman.mardiyanto@gmail.com>
2021-04-19 14:51:24 +0700

    statis

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  // target: 'static',
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -82,8 +82,8 @@ export default {
     //     component: resolve(__dirname, 'pages/Pengujian/Pendaftaran.vue')
     //   })
     // }
+  },
+  generate: {
+    dir: '../labkon'
   }
-  // generate: {
-  //   dir: '../labkon'
-  // }
 }
