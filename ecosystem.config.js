f0ebf2db3690285c7d326a615996827e16bb283f
firmanmardiyanto <firman.mardiyanto@gmail.com>
2021-04-19 14:26:00 +0700

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Labkon',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
