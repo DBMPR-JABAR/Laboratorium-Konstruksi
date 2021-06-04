271d829ba82b7b3c6eb7eaa1d0de446c656a2a3e
firmanmardiyanto <firman.mardiyanto@gmail.com>
2021-06-04 10:23:52 +0700

    sm

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,110 +1,110 @@
 import webpack from 'webpack'
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: process.env.TARGET || 'static',
-  publicRuntimeConfig: {
-    storageUrl: process.env.STORAGE_URL || 'http://124.81.122.132/temanjabar/public/storage/'
-  },
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Labolatorium Kontruksi',
-    htmlAttrs: {
-      lang: 'id'
+    // Target: https://go.nuxtjs.dev/config-target
+    target: process.env.TARGET || 'static',
+    publicRuntimeConfig: {
+        storageUrl: 'http://124.81.122.132/temanjabar/public/storage/'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Bina Marga Laboratorium Konstruksi' }
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'Labolatorium Kontruksi',
+        htmlAttrs: {
+            lang: 'id'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Bina Marga Laboratorium Konstruksi' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['~node_modules/@coreui/coreui/scss/coreui',
+        '~assets/scss/style.scss',
+        '~node_modules/vue-select/src/scss/vue-select.scss',
+        '~node_modules/vue-tour/dist/vue-tour.css'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~node_modules/@coreui/coreui/scss/coreui',
-    '~assets/scss/style.scss',
-    '~node_modules/vue-select/src/scss/vue-select.scss',
-    '~node_modules/vue-tour/dist/vue-tour.css'
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [{ src: '~/plugins/coreui-vue.js', mode: 'client' },
+        { src: '~/plugins/icons.js', mode: 'client' },
+        { src: '~/plugins/vue-select.js', mode: 'client' },
+        { src: '~/plugins/html-to-paper.js', mode: 'client' },
+        { src: '~/plugins/vue-pdf.js', mode: 'client' },
+        { src: '~/plugins/bootstrap-vue.js', mode: 'client' },
+        { src: '~/plugins/nuxt-leaflet.js', mode: 'client' },
+        { src: '~/plugins/vue-tour.js', mode: 'client' },
+        { src: '~/plugins/lingallery', mode: 'client' }
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/coreui-vue.js', mode: 'client' },
-    { src: '~/plugins/icons.js', mode: 'client' },
-    { src: '~/plugins/vue-select.js', mode: 'client' },
-    { src: '~/plugins/html-to-paper.js', mode: 'client' },
-    { src: '~/plugins/vue-pdf.js', mode: 'client' },
-    { src: '~/plugins/bootstrap-vue.js', mode: 'client' },
-    { src: '~/plugins/nuxt-leaflet.js', mode: 'client' },
-    { src: '~/plugins/vue-tour.js', mode: 'client' },
-    { src: '~/plugins/lingallery', mode: 'client' }
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module'
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+        '@nuxtjs/auth',
+        'nuxt-leaflet'
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/auth',
-    'nuxt-leaflet'
-  ],
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        baseUrl: process.env.API_URL || 'http://localhost/temanjabar/public/api/'
+    },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    baseUrl: process.env.API_URL || 'http://localhost/temanjabar/public/api/'
-  },
-
-  // Authorization
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: { url: 'auth/login', method: 'post', propertyName: 'data.token.access_token' },
-          user: { url: 'auth/user', method: 'post', propertyName: 'data.user' },
-          logout: false
+    // Authorization
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: { url: 'auth/login', method: 'post', propertyName: 'data.token.access_token' },
+                    user: { url: 'auth/user', method: 'post', propertyName: 'data.user' },
+                    logout: false
+                }
+            }
         }
-      }
-    }
-  },
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    plugins: [
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      })
-    ],
-    transpile: ['@coreui']
-  },
+    },
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            })
+        ],
+        transpile: ['@coreui']
+    },
 
-  router: {
-    base: String(process.env.TARGET) === 'server' ? '/' : '/labkon/',
-    // mode: 'hash',
-    linkActiveClass: 'active',
-    routeNameSplitter: '/',
-    middleware: ['auth']
-    // extendRoutes (routes, resolve) {
-    //   routes.push({
-    //     name: 'Pengujian',
-    //     path: 'pengujian',
-    //     redirect: '/pengujian/pendaftaran',
-    //     // component: {
-    //     //   render (c) { return c('router-view') }
-    //     // }
-    //     component: resolve(__dirname, 'pages/Pengujian/Pendaftaran.vue')
-    //   })
-    // }
-  },
-  generate: {
-    dir: String(process.env.TARGET) === 'server' ? './dist' : '../labkon'
-  }
+    router: {
+        base: String(process.env.TARGET) === 'server' ? '/' : '/labkon/',
+        // mode: 'hash',
+        linkActiveClass: 'active',
+        routeNameSplitter: '/',
+        middleware: ['auth']
+            // extendRoutes (routes, resolve) {
+            //   routes.push({
+            //     name: 'Pengujian',
+            //     path: 'pengujian',
+            //     redirect: '/pengujian/pendaftaran',
+            //     // component: {
+            //     //   render (c) { return c('router-view') }
+            //     // }
+            //     component: resolve(__dirname, 'pages/Pengujian/Pendaftaran.vue')
+            //   })
+            // }
+    },
+    generate: {
+        dir: String(process.env.TARGET) === 'server' ? './dist' : '../labkon'
+    }
 }
